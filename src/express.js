10f7e9691ff1074d1fb647cb605455d0d8347f11
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-12-29 01:32:05 +0100

    Add .eslintignore & optimize production runtime

--- src/express.js
+++ src/express.js
@@ -6,16 +6,16 @@ import { config } from './config'
 const app = express()
 const router = express.Router()
 app.use('/api', router)
-switch (config.APP_ENV) {
-  case 'dev':
-    app.use(morgan('dev'))
+switch (process.env.NODE_ENV) {
+  case 'production':
+    app.use(morgan('combined'))
     break
 
   default:
-    app.use(morgan('combined'))
+    app.use(morgan('dev'))
     break
 }
-app.set('port', config.PORT || 3000)
+app.set('port', config.PORT || process.env.PORT || 3000)
 app.use(express.static(path.join(__dirname, '../public')))
 
 export { app, router }
